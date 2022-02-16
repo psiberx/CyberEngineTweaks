d3695b60526d5d63ba0c4e6eb99e90502773b876
pv <p@siberx.dev>
2022-02-16 15:05:36 +0300

    Fix start screen patch

--- src/patches/SkipStartScreen.cpp
+++ src/patches/SkipStartScreen.cpp
@@ -13,12 +13,15 @@ void StartScreenPatch(const Image* apImage)
         return;
     }
 
-    pLocation -= 9;
-
     DWORD oldProtect = 0;
     VirtualProtect(pLocation, 32, PAGE_EXECUTE_WRITECOPY, &oldProtect);
-    pLocation[0] = 0x90;
-    pLocation[1] = 0x90;
+    pLocation[0] = 0xE9;
+    pLocation[1] = 0x08;
+    pLocation[2] = 0x01;
+    pLocation[3] = 0x00;
+    pLocation[4] = 0x00;
+    pLocation[5] = 0x00;
+    pLocation[6] = 0x00;
     VirtualProtect(pLocation, 32, oldProtect, nullptr);
 
     Log::Info("Start screen patch: success");
